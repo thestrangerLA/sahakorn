750436fc9cff21005b3054bdb69418b7c4e5f0d9
Phoumiphon THAMMAVONG <teepmp1302@gmail.com>
2026-01-15 15:10:46 +0000

    ເຊື່ອມຕໍ່ລະບົບເຂົ້າກັບ database ທີ່ມີຢູ່ແລ້ວ ແລະ ສາມາດບັນທຶກຂໍ້ມູນ

--- home/runner/work/tee-work/tee-work/src/lib/firebase.ts
+++ home/runner/work/tee-work/tee-work/src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "kaset-stock-manager",
-  "appId": "1:772228625471:web:93f2f9fa68522ec202748a",
-  "storageBucket": "kaset-stock-manager.appspot.com",
   "apiKey": "AIzaSyBdsGQbRWWD1lafromwVCjPQmAQT9bGboU",
   "authDomain": "kaset-stock-manager.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "772228625471"
+  "projectId": "kaset-stock-manager",
+  "storageBucket": "kaset-stock-manager.appspot.com",
+  "messagingSenderId": "772228625471",
+  "appId": "1:772228625471:web:93f2f9fa68522ec202748a"
 };
 
 // Initialize Firebase app as a singleton
